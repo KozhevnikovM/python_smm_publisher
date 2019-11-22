49018233eb40461f39da3b45b9b65ad8de5a53b1
Administrator <admin@example.com>
2019-11-22 11:08:53 +0300

    Moved constants in config

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from vk import vk_post
 from tg import tg_send_to_chanel
 from facebook import fb_post
+from config import VK_GROUP_ID, VK_ALBUM_ID, TG_CHANEL, FB_GROUP_ID
 
 
 if __name__ == '__main__':
@@ -10,13 +11,6 @@ if __name__ == '__main__':
     photos_dir = os.path.join(os.path.dirname(__file__), 'uploads', 'photos')
     attachment = os.path.join(photos_dir, filename) if filename else None
     os.makedirs(photos_dir, exist_ok=True)
-
-    VK_GROUP_ID = 181016321
-    VK_ALBUM_ID = 262494951
-
-    TG_CHANEL = '@python_auto_post'
-
-    FB_GROUP_ID = '4062480697102376'
 
     if not message and not filename:
         exit('No message to post')

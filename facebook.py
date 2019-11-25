b5f3f918df6b223fd98be742fbf9fc3b919de2ad
Administrator <admin@example.com>
2019-11-25 11:41:38 +0300

    added response checking

--- facebook.py
+++ facebook.py
@@ -3,6 +3,9 @@ import requests
 
 API_URL = 'https://graph.facebook.com/v5.0'
 
+def check_fb_response(json):
+    if 'error' in json:
+        raise Exception(json['error'])
 
 def fb_post_text_only(group_id, message):
     url = f'{API_URL}/{group_id}/feed'
@@ -10,19 +13,25 @@ def fb_post_text_only(group_id, message):
         'access_token': FB_ACCESS_TOKEN,
         'message': message
     }
-    return requests.post(url, data=data).content
+    response = requests.post(url, data=data)
+    check_fb_response(response.json())
+    return response.content
 
 
 def fb_post(group_id, attachment=None, caption=None):
     if not attachment:
         return fb_post_text_only(group_id, caption)
     url = f'{API_URL}/{group_id}/photos'
-    files = {'upload_file': open(attachment, 'rb')}
     data = {
         'access_token': FB_ACCESS_TOKEN,
         'caption': caption
     }
-    return requests.post(url, data=data, files=files).content
+    with open(attachment, 'rb') as file:
+        files = {'upload_file': file}
+        response = requests.post(url, data=data, files=files)
+    print(response.json())
+    check_fb_response(response.json())
+    return response.content
 
 
 if __name__ == '__main__':
